1e2f0034cc6d12f72f2efbac891eaffc4b164fc9
janavarro8 <43156812+janavarro8@users.noreply.github.com>
2021-09-02 03:24:12 -0600

    Including time and blocked function
    blocked function has an index out of bounds bug

--- serialMatrixMultiply.py
+++ serialMatrixMultiply.py
@@ -1,4 +1,5 @@
 from matrixUtils import *
+import time
 
 def main():
     size = input("Enter the size of the matrices you would like to use:\n")
@@ -9,8 +10,18 @@ def main():
     m1 = genMatrix(int(size), int(m1Value))
     m2 = genMatrix(int(size), int(m2Value))
 
+    multTime1 = time.monotonic()
     product = matrixMult(m1, m2)
+    multTime1 = time.monotonic() - multTime1
     printSubarray(product)
+    print(multTime1)
+
+    multTime2 = time.monotonic()
+    product2 = blockedMatMult(m1, m2)
+    multTime2 = time.monotonic() - multTime2
+    printSubarray(product2)
+    print(multTime2)
+    
 
 """
 Multiplies 2 given matrices and returns the product
@@ -24,6 +35,26 @@ def matrixMult(m1, m2):
             for k in range(length): # used for both matrices
                 product[i][j] += m1[i][k] * m2[k][j]
     return product
+
+"""
+Multiples 2 given matrices using a step
+"""
+def blockedMatMult(m1, m2):
+    length = len(m1)
+    tile_size = 16
+    output = genMatrix(length, 0)
+    sum = 0
+
+    for kk in range(0, length, tile_size):
+        for jj in range(0, length, tile_size):
+            for i in range(length): 
+                j_end_val = jj + tile_size 
+                for j in range(jj, j_end_val): 
+                    k_end_val = kk + tile_size 
+                    sum = output[i][j] 
+                    for k in range(kk, k_end_val): 
+                        sum = sum + m1[i][k] * m2[k][j] 
+                    output[i][j] = sum 
 
 if __name__ == '__main__':
     main()
